947cff23ee128cdeed017c290c3ca6122a2f0949
Thin Wut Hmone Htoo <thinwuthmonehtoo@Thins-MacBook-Air.local>
2024-06-27 21:28:20 +0630

    asp.net movielist tuto

---
+++ MovieList/Migrations/20240623034950_rating_to_movie_model.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieList.Migrations
+{
+    /// <inheritdoc />
+    public partial class rating_to_movie_model : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
